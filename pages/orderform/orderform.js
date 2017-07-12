3d809711f7ae1262bb8568c47257d3b265ddaf8a
YumleeWang <543265835@qq.com>
2017-07-12 18:49:35 +0800

    7/12
    页面制作

---
+++ pages/orderform/orderform.js
@@ -0,0 +1,18 @@
+// pages/orderform/orderform.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ordertype: 'jd',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+})
---
+++ pages/orderparticulars/orderparticulars.js
@@ -0,0 +1,24 @@
+// pages/orderparticulars/orderparticulars.js
+Page({
+  data: {
+    upmore:true,
+  },
+
+  onLoad: function (options) {
+  
+  },
+  // 查看全部已点菜品
+  upmore: function () {
+    var that = this;
+    that.setData({
+      upmore: (!that.data.upmore)
+    })
+  },
+  // 拨打电话
+  call:function(){
+    var that = this;
+    wx.makePhoneCall({
+      phoneNumber: '' //仅为示例，并非真实的电话号码
+    })
+  },
+})
